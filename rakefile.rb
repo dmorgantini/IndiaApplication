ff01901a4ce311aaffdc007884b94a7a3c72f80b
dmorgantini@gmail.com <David@David-PC.(none)>
2012-02-12 21:05:11 +0000

    added package step

--- rakefile.rb
+++ rakefile.rb
@@ -15,3 +15,17 @@ nunit :test do |nunit|
 	nunit.assemblies "IndiaApplication.unit.test/bin/Debug/IndiaApplication.unit.test.dll"
 end
 
+desc "Package application"
+zip :package, [:build_number] => :copy_to_output do |zip, args| 
+    zip.directories_to_zip "build"
+    zip.output_file = "IndiaApplication.v#{args["build_number"]}.zip"
+    zip.output_path = File.dirname(__FILE__) + "/build"
+end
+
+msbuild :copy_to_output do |msb|
+	msb.properties  :configuration => :Release, "_PackageTempDir" => "../build", "AutoParameterizationWebConfigConnectionStrings" => :false
+	msb.targets :Build, :PipelinePreDeployCopyAllFilesToOneFolder
+	msb.solution = "IndiaApplication/IndiaApplication.csproj"
+end
+
+
